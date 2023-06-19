d0807fb6974f4b5b567dbbc38c42ca660dabd866
emma wambui <emma.wambui@student.moringaschool.com>
2023-06-19 08:54:34 +0300

    final final commit

--- challange2.js
+++ challange2.js
@@ -3,21 +3,24 @@
 const speedLimit = 70
 const kmPerSpeedDemiritPoints = 5
 const maxSpeedLimitDemeritPoints = 12
-const demiritPoints = ((speed-speedLimit)/kmPerSpeedDemiritPoints)
+ let demeiritPoints  
 
 function speedDetector(speed){
 
 if (speed <= speedLimit){
     console.log('OK');
+    // speed > speedLimit
 
-}else if( demiritPoints <= maxSpeedLimitDemeritPoints){
-    console.log('demeritsPoints');
-}else ( demiritPoints => maxSpeedLimitDemeritPoints);{
+}else if(speed > speedLimit ){
+    demeiritPoints = (speed-speedLimit) / kmPerSpeedDemiritPoints
+    
+    console.log(demeiritPoints);
+}else if( demeiritPoints > maxSpeedLimitDemeritPoints);{
     console.log('License suspended');
 }
 }
-const speed = parseInt('90')
-function speedDetector(speed)
-console.log(speedDetector(speed));
+// const speed = parseInt('90')
+// speedDetector(75)
+console.log(speedDetector(80));
 
  
